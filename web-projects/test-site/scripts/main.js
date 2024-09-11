c7fc57666ab18c1289b7aebc7ad4908eac8e1090
Alec Mirambeau <alecmirambeau@gmail.com>
2024-09-11 14:06:07 -0400

    Gave button functionality

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -8,3 +8,38 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/library-three-pane-window.jpeg");
   }
 };
+
+
+// Start of code to assign user name
+let myButton = document.querySelector("button")
+let myHeading = document.querySelector("h1")
+
+// function to set the user name
+function setUserName(){
+    const myName = prompt("Please enter your name.");
+    if (myName == " " || myName === " " || !myName){
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Welcome to Miriam's library, ${myName}`;
+        }
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
+// End of code to assign user name
+
+
+// Function to initialize the website
+function loadPreviousNameIfSaved(){
+    console.log(`stored user name value is: ${localStorage.getItem("name")}!`)
+    if (!localStorage.getItem("name")){
+        setUserName();
+    } else {
+        const storedName = localStorage.getItem("name");
+        myHeading.textContent = `Welcome to Miriam's library, ${storedName}`;
+    }
+}
+
+loadPreviousNameIfSaved();
